c71953b08259986697e1d75240c0882596452285
Rajan Dhariyaparmar <rajandalvadi2510@gmail.com>
2024-12-26 23:27:16 +0530

    q

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-})
+  base: '/', // Ensure this matches your deployment root
+  build: {
+    outDir: 'dist', // Default output folder for Vite
+  }
+});
